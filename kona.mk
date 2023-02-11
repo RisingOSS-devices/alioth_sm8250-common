2dc92d4cfd5d7918524cc4f406bf6475a825167a
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2023-02-11 08:47:08 +0100

    sm8250-common :  Build mock hidl power stats
    * Fix for hidl power stats errors.
    * New updated legacy-um sepolicy solved the android.hardware.power@1.0::IPower/default error
    - Now with this we fixing both errors:
    BatteryStatsService: Unable to load Power Hal or power.stats HAL
    hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.

--- kona.mk
+++ kona.mk
@@ -497,7 +497,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+ # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
